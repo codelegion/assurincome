d85b975e8f7872d901fcd6dd1d8b2fdb0a895355
Andrew Altamirano <slogen123@users.noreply.github.com>
2017-11-05 01:37:23 -0400

    add app routes

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 let express 	= require('express');
 let app 		= express();
 let port		= process.env.PORT || 3000;
-let router		= express.Router();
+
+let bodyParser	= require('body-parser');
+let calculator	= require('./calculator');
+
+app.use(express.static('static'));
+app.use(bodyParser.json());
+
+
+app.get('/', function(req, res) {
+	res.sendFile('static/index.html');
+});
+
+app.get('/option2', function(req, res) {
+	res.sendFile('static/option2.html', {
+		root: __dirname
+	});
+});
+app.get('/option3', function (req, res) {
+	res.sendFile('static/option3.html', {
+		root: __dirname
+	});
+});
+
+app.post('/calculate', function(req, res) {
+
+	let args = [req.body.termLength, req.body.remitRate100M, req.body.remitRate10M, req.body.remitRate1M, req.body.remitRate100K, req.body.remitRate75K, req.body.remitRate50K];
+	let rate = calculator.calculatePayment(...args);
+	res.status(rate.status).json(rate);
+});
 
 app.listen(port);
